45ef31cd579e44ff856bddc78a62094856497b46
altairFonAlfheim <insania_officialis@outlook.com>
2025-08-06 08:58:59 +0300

    -Начало рефакторинга страницы карты -Разделение инициализации на подфукнции -Добавление запросов стран и координат стран -Добавление вывода координат стран

---
+++ wwwroot/js/api/politics_api.js
@@ -0,0 +1,75 @@
+﻿//Объявление переменной ссылки на api работы с политикиой
+const baseUrlPoliticsRead = 'http://192.168.31.234:7083/';
+
+//Функция получения стран
+async function getCountriesList(has_coordinates = true) {
+    try {
+        //Формирование строки запроса
+        const url = new URL(baseUrlPoliticsRead + 'countries/list');
+
+        //Добавление query-параметров
+        url.searchParams.append('has_coordinates', has_coordinates);
+
+        //Отправка запроса
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        //Проверка статуса ответа
+        if (!response.ok) throw new Error(`Некорректный статус ответа: ${response.status}`);
+
+        //Преобразование ответа в json
+        const data = await response.json();
+
+        //Проверка структуры ответа
+        if (!data?.success) throw new Error(`Неуспешный ответ: ${response.message}`);
+        if (!data?.items?.length) throw new Error('Не указаны географические объекты');
+
+        //Возврат ответа
+        return data.items;
+    } catch (error) {
+        //Вывод ошибки
+        console.error('Ошибка:', error);
+    }
+}
+
+//Функция получения координат стран
+async function getCountriesCoordinatesList(countryId = null) {
+    try {
+        //Проверки
+        if (countryId === null && countryId === undefined) throw new Error('Не указан идентификатор страны');
+
+        //Формирование строки запроса
+        const url = new URL(baseUrlPoliticsRead + 'countries_coordinates/list');
+
+        //Добавление query-параметров
+        url.searchParams.append('country_id', countryId);
+
+        //Отправка запроса
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        //Проверка статуса ответа
+        if (!response.ok) throw new Error(`Некорректный статус ответа: ${response.status}`);
+
+        //Преобразование ответа в json
+        const data = await response.json();
+
+        //Проверка структуры ответа
+        if (!data?.success) throw new Error(`Неуспешный ответ: ${response.message}`);
+        if (!data?.items?.length) throw new Error('Не указаны координаты географического объекта');
+
+        //Возврат ответа
+        return data;
+    } catch (error) {
+        //Вывод ошибки
+        console.error(`Ошибка: ${error}`);
+    }
+}
