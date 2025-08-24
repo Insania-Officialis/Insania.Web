279918646614f1e644c6a01bcc93a4818e3a4f91
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-08-24 15:01:35 +0300

    - Добавление в запросе получения географических объектов типа рек - Добавление вывода в консоль уровня масштабирования

--- wwwroot/js/api/geography_api.js
+++ wwwroot/js/api/geography_api.js
@@ -3,7 +3,7 @@ const baseUrlGeographyRead = 'http://192.168.1.104:7086/';
 const baseUrlGeographyCommit = 'http://192.168.1.104:7087/';
 
 //Функция получения географических объектов
-async function getGeographyObjectsList(has_coordinates = true, type_ids = [4,6]) {
+async function getGeographyObjectsList(has_coordinates = true, type_ids = [4,6,8]) {
     try {
         //Формирование строки запроса
         const url = new URL(baseUrlGeographyRead + 'geography_objects/list');
